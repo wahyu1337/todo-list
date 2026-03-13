ecf5737c585fcac7c6a01da9a3070acc7da0ff37
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-03-13 16:29:12 +0800

    Refactor project.js and update index.js

--- src/classes/project.js
+++ src/classes/project.js
@@ -2,24 +2,30 @@ import { logsMessage } from "./../logs.js";
 import { Todo } from "./todo.js";
 
 class Project {
-    constructor (title, description) {
+    constructor (title) {
         this.title = title;
-        this.description = description;
-        this.todos = [];
+        this.detail = [];
     }
     // add 
     addTask (title, description, dueDate, priority, isDone) {
         // add to Todo Logic
         const task = new Todo(title, description, dueDate, priority, isDone);
         // push into addTask
-        this.todos.push(task);
+        this.detail.push(task);
+        logsMessage("New Task Has been added!");
     }
     // remove 
     removeTask (task) {
-        this.todos = this.todos.filter(item => item !== task);
+        this.detail = this.detail.filter(item => item !== task);
+        logsMessage(`${task} has been remove from task!`)
     }
     // search
     searchTask (task) {
-        return this.todos.find(item => item === task);
+        logsMessage("Searching task...")
+        const findItem = this.detail.find(item => item === task) 
+        logsMessage(`Task founded. ${task}`);
+        return findItem;
     }
 }
+
+export {Project};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {Project} from "./classes/project.js";
+
+const task = new Project("Olahraga" );
+task.addTask("Jogging", "Simple Run in morning", "14-03-2026", "Low");
+console.log(task);
