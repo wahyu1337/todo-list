2690056a69e22efb5a5250c35b5283dbbdbe094f
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-03-16 20:11:32 +0800

    Refactor on indexJS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // import module
-import {Project} from "./classes/project.js";
-import {renderHeader, renderSidebar, renderMainContent, modalNewProject} from "./dom.js";
+import {renderHeader, renderSidebar, renderMainContent} from "./dom.js";
 import style from "./styles/style.css";
-import { addModalBox, modalBox } from "./event-handler.js";
+import { addProjects } from "./event-handler.js";
 
 renderHeader();
 renderSidebar();
 renderMainContent();
 
 // event handler
-addModalBox();
+addProjects();
